20c1f7522b52ced8124eb24994ae7aa99271a4f9
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2012-03-17 17:58:48 +0800

    base algorithms done

--- 10038/10038.cc
+++ 10038/10038.cc
@@ -1,5 +1,9 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
+#include <stdlib.h>
+
+bool is_jolly(const std::vector<long>& jumpers);
 
 int main() {
   long number_of_jumpers;
@@ -12,10 +16,29 @@ int main() {
 	std::cin>>jumper;
 	jumpers.push_back(jumper);
       }
+      std::cout << is_jolly(jumpers) << std::endl;
+    } else {
+      std::cout << number_of_jumpers << std::endl;
     }
-    std::cout << number_of_jumpers << std::endl;
   }
 }
 
-bool is_jolly(const &vector<long> jumpers){
-  jumpers.
+bool is_jolly(const std::vector<long>& jumpers){
+  std::vector<long> jumper_diffs;
+  for(int i=0; i<jumpers.size()-1; i++){
+    jumper_diffs.push_back(
+        abs(
+	    jumpers[i]-jumpers[i+1]));
+  }
+  std::sort(jumper_diffs.begin(), jumper_diffs.end());
+  for(int i=0; i<jumper_diffs.size(); i++){
+    std::cout<<jumper_diffs[i]<<" ";
+  }
+  std::cout<<std::endl;
+  for(int i=1; i <= jumper_diffs.size(); i++){
+    if (jumper_diffs[i-1] != i){
+      return false;
+    }
+  }
+  return true;
+}
